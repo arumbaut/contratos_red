c037aafcef0c4cf6abf36db7cf3fa4fc9b767e26
Adrian Alonso Rumbaut <aalonso1190@gmail.com>
2021-05-13 13:40:11 -0400

    Creando mi tabla de servicios y datos

---
+++ src/components/P_Table.vue
@@ -0,0 +1,134 @@
+<template>
+  <q-page >
+    <div class="row">
+      <div class="col">Fecha Inicio:</div>
+      <div class="col" v-for="(value,index) in fechaI" :key="index">
+        {{ value }}
+        <q-popup-edit auto-save v-model="fechaI[index]" content-class="bg-blue-5 text-red">
+          <q-input   type="date" dark color="white" v-model="fechaI[index]" dense autofocus counter>
+          </q-input>
+        </q-popup-edit>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col">Fecha Fin:</div>
+      <div class="col" v-for="(value,index) in fechaF" :key="index">
+        {{ value }}
+        <q-popup-edit auto-save v-model="fechaF[index]" content-class="bg-blue-5 text-red">
+          <q-input   type="date" dark color="white" v-model="fechaF[index]" dense autofocus counter>
+
+          </q-input>
+        </q-popup-edit>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">Servicio de Correo:</div>
+      <div class="col" v-for="(value,index) in servCorreo" :key="index">
+        {{ value }}
+        <q-popup-edit auto-save v-model="servCorreo[index]" content-class="bg-blue-5 text-red">
+          <q-select :options=options  color="white" v-model="servCorreo[index]" dense  counter hint="Servicio"></q-select>
+        </q-popup-edit>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">Cuota de Correo:</div>
+      <div class="col" v-for="(value,index) in cuotaCorreo" :key="index">
+        {{ value }}
+        <q-popup-edit auto-save v-model="cuotaCorreo[index]" content-class="bg-blue-5 text-red">
+          <q-select :options=options  color="white" v-model="cuotaCorreo[index]" dense  counter></q-select>
+        </q-popup-edit>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">Servicio de Internet:</div>
+      <div class="col" v-for="(value,index) in servInternet" :key="index">
+        {{ value }}
+        <q-popup-edit auto-save v-model="servInternet[index]" content-class="bg-blue-5 text-red">
+          <q-select :options=options  color="white" v-model="servInternet[index]" dense  counter hint="Servicio"></q-select>
+        </q-popup-edit>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">Cuota de Internet:</div>
+      <div class="col" v-for="(value,index) in cuotaInternet" :key="index">
+        {{ value }}
+        <q-popup-edit auto-save v-model="cuotaInternet[index]" content-class="bg-blue-5 text-red">
+          <q-select :options=options  color="white" v-model="cuotaInternet[index]" dense  counter hint="Cuota"></q-select>
+        </q-popup-edit>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">Número de Teléfono:</div>
+      <div class="col" v-for="(value,index) in nunTelefono" :key="index">
+        {{ value }}
+        <q-popup-edit auto-save v-model="nunTelefono[index]" content-class="bg-blue-5 text-red">
+          <q-input type="tel" dark color="white" v-model="nunTelefono[index]" dense autofocus counter hint="Telephone number">
+          </q-input>
+        </q-popup-edit>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">Otros Servisio:</div>
+      <div class="col" v-for="(value,index) in otrosServicios" :key="index">
+        {{ value }}
+        <q-popup-edit auto-save v-model="otrosServicios[index]" content-class="bg-blue-5 text-red">
+          <q-input dark color="white" v-model="otrosServicios[index]" dense autofocus counter hint="Servicio">
+
+          </q-input>
+        </q-popup-edit>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">Firma Jefe de Área:</div>
+      <div class="col" v-for="(value,index) in firmajArea" :key="index">
+        {{ value }}
+        <q-popup-edit auto-save v-model="firmajArea[index]" content-class="bg-blue-5 text-red">
+          <q-input dark color="white" v-model="firmajArea[index]" dense autofocus counter hint="Firma"></q-input>
+        </q-popup-edit>
+      </div>
+    </div>
+    {{fechaI}}
+    {{fechaF}}
+    {{servCorreo}}
+    {{cuotaCorreo}}
+    {{servInternet}}
+    {{cuotaInternet}}
+    {{nunTelefono}}
+    {{otrosServicios}}
+    {{firmajArea}}
+
+  </q-page>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      fechaI: new Array(8),
+      fechaF: new Array(8),
+      servCorreo: new Array(8),
+      cuotaCorreo: new Array(8),
+      servInternet: new Array(8),
+      cuotaInternet: new Array(8),
+      nunTelefono: new Array(8),
+      otrosServicios: new Array(8),
+      firmajArea: new Array(8),
+      model: null,
+      options: [
+        'L-Local', 'R-Cuba', 'F-Libre'
+      ]
+
+    }
+  }
+
+}
+</script>
+
+<style lang="sass" scoped>
+.row > div
+  padding: 10px 15px
+  border: 1px solid rgba(86,61,124,.2)
+
+
+</style>
--- src/pages/C_Colectivo.vue
+++ src/pages/C_Colectivo.vue
@@ -5,47 +5,89 @@
     </div>
     <form action="">
       <div class="row q-col-gutter-sm q-mt-md">
-        <div class="col-4">
+        <div class="col-12 col-sm-4">
           <div class="my-content">
-            <q-select  dense v-model="select" :options="options" label="Área">
-              <q-tooltip content-class="bg-grey-8" anchor="top left" self="bottom left" :offset="[0, 8]">Administrador seleccione un área</q-tooltip>
+            <q-select color="green-7" dense v-model="select" :options="options" label="Área">
+              <q-tooltip content-class="bg-grey-8" anchor="top left" self="bottom left" :offset="[0, 8]">Administrador
+                seleccione un área
+              </q-tooltip>
             </q-select>
           </div>
         </div>
-        <div class="col-4">
+        <div class="col-12 col-sm-4">
           <div class="my-content">
-            <q-input dense v-model="ano" label="Año" />
+            <q-input color="green-7" dense v-model="ano" label="Año"/>
           </div>
         </div>
-        <div class="col-4">
+        <div class="col-12 col-sm-4">
           <div class="my-content">
-            <q-input dense v-model="mes" label="Mes" />
+            <q-input color="green-7" dense v-model="mes" label="Mes"/>
           </div>
         </div>
-        <div class="col-4">
-          <div class="my-content">
-            <q-input dense type="number" v-model.number="conIndividual" label="Orden de contrato individual No:" />
+        <div class="col-12 col-sm-4">
+          <div class="col">
+            <div class="my-content">
+              <q-input color="green-7" dense type="number" v-model.number="conIndividual" label="Orden de contrato individual No:"/>
+            </div>
+          </div>
+          <div class="col">
+            <div class="my-content">
+              <q-input color="green-7" dense type="number" v-model.number="conColectivos" label="Orden de contrato colectivo No:"/>
+            </div>
+          </div>
+          <div class="col">
+            <div class="my-content">
+              <q-input color="green-7" dense type="number" v-model.number="ordInicial" label="Número de orden inicial"/>
+            </div>
+          </div>
+
+        </div>
+
+       <div class="col-12 col-sm-8">
+         <div class="col">
+           <div class="my-content">
+             <q-input color="green-7" dense type="number" v-model.number="cUsers" label="Contrato Colectivo No"/>
+           </div>
+         </div>
+
+         <div class="col">
+           <div class="my-content">
+             <q-input color="green-7" dense type="number" v-model.number="cUsers" label="Cantidad de usuarios"/>
+           </div>
+         </div>
+         <div class="col">
+           <div class="my-content">
+             <q-input  color="green-7" dense v-model="text" label="Tipo de curso"/>
+           </div>
+         </div>
+       </div>
+        <div class="col text-h4 text-weight-light text-center q-pt-lg text-green-7 " style="margin: auto">
+          Servicios Autorizados:
+          <div class="text-body2 text-justify text-black">
+            La fecha final de cada actualización debe ser el 22 de septiembre del inicio del curso siguiente <br/>
+            Correo -- [Seleccionar si correo local (L), solo Cuba (R) o Libre (F)] <br/>
+            Internet -- [Seleccionar 1 si internet solo Cuba, 3 Est. Y Trab., 3 Profesor, 4 Autorizados x CD]
           </div>
         </div>
-        <div class="col-4">
-          <div class="my-content">
-            <q-input dense type="number" v-model.number="conColectivos" label="Orden de contrato colectivo No:" />
-          </div>
-        </div>
-        <div class="col-4">
-          <div class="my-content">
-            <q-input dense type="number" v-model.number="ordInicial" label="Número de orden inicial" />
-          </div>
-        </div>
-        <div class="col-4">
-          <div class="my-content">
-            <q-input dense type="number" v-model.number="cUsers" label="Cantidad de usuarios" />
-          </div>
-        </div>
-        <div class="col-4">
-          <div class="my-content">
-            <q-input dense v-model="text" label="Tipo de curso" />
-          </div>
+        <div class="col-12">
+          <!--<q-markup-table
+            separator="cell"
+            flat bordered
+          >
+            <tbody>
+              <tr v-for="(name,index) in tableValue" :key="index">
+                <td class="text-left">{{ name.name }}</td>
+                <td class="text-right">{{ index }}</td>
+                <td class="text-right">{{ index }}</td>
+                <td class="text-right">{{ index }}</td>
+                <td class="text-right">{{ index }}</td>
+                <td class="text-right">{{ index }}</td>
+                <td class="text-right">{{ index }}</td>
+                <td class="text-right">{{ index }}</td>
+              </tr>
+            </tbody>
+          </q-markup-table>-->
+          <P_Table />
         </div>
       </div>
     </form>
@@ -53,22 +95,26 @@
   </q-page>
 </template>
 
+
 <script>
+import P_Table from "components/P_Table";
 var fecha = new Date();
 export default {
   name: "C_Colectivo",
-  data(){
-    return{
-      cUsers:1,
+  components:{P_Table},
+  data() {
+    return {
+      cUsers: 1,
       conColectivos: 1,
       conIndividual: 1,
       ordInicial: 1,
       ano: fecha.getFullYear(),
-      mes : fecha.getMonth(),
-      options : ['ASSETS','CRAI','CUMAB','CUMAG','CUMCC','CUMCF','CUMCY'],
-      select: null
-
-
+      mes: fecha.getMonth(),
+      options: ['ASSETS', 'CRAI', 'CUMAB', 'CUMAG', 'CUMCC', 'CUMCF', 'CUMCY'],
+      select: null,
+      tableValue: [ {name: 'Fecha Inicio:'},{name: 'Fecha Fin:'},{name: 'Servicio Correo:'},{name: 'Cuota Correo:'},{name: 'Servicio Internet:'},
+        {name: 'Cuota Internet:'},{name: 'Nro teléfono:'},{name: 'Otros Servicios:'},{name: 'Firma Area'}
+      ]
     }
   }
 }
@@ -77,5 +123,6 @@ export default {
 <style lang="sass" scoped>
 .my-content
   padding: 10px 15px
+
 
 </style>
