2feb0bbdf3fcba9882c21a3c1caea5cc29043a24
Adrian Aloso Rumbaut <aalonso1190@gmail.com>
2021-05-18 10:01:53 -0400

    Creando New Login

--- src/layouts/Login.vue
+++ src/layouts/Login.vue
@@ -1,17 +1,16 @@
 <template>
   <q-layout view="hHh Lpr fFf"> <!-- Be sure to play with the Layout demo on docs -->
-
-   <q-page-container>
-      <router-view />
-   </q-page-container> 
+    <q-page-container>
+      <router-view/>
+    </q-page-container>
   </q-layout>
 </template>
 
 <script>
+
 export default {
   // name: 'LayoutName',
-
-  data () {
+  data() {
     return {
       leftDrawer: true
     }
--- src/pages/P_Login.vue
+++ src/pages/P_Login.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-page class=" window-height window-width row justify-center items-center">
+  <q-page class=" window-height window-width row justify-center items-center" :style="{'background-image': 'url(' + require('assets/bg.jpg') + ')'}">
       <div class="column ">
       <div class="row flex-center bg-light-blue-4 shadow-up-5">
         <h5 class="text-h5 text-white q-my-md">Sing In</h5>
